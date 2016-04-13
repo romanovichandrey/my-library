1a7b6f51e76feb1ca633f9ce65106e0d47583931
romanovichandrey <romanovichandrey1989@gmail.com>
2016-04-13 08:35:30 +0300

    this is first commit

---
+++ my-domain/src/main/java/by/romanovich/it/pojos/users/Citys.java
@@ -0,0 +1,64 @@
+package by.romanovich.it.pojos.users;
+
+import java.io.Serializable;
+
+/**
+ * This is Citys pojo
+ * @author Romanovich Andrei
+ * @version 1.0
+ */
+public class Citys implements Serializable {
+
+    private static final Long serialVersionUID = 13409278437L;
+
+    /**@serial (name = id_city)*/
+    private Integer idCity;
+
+    /**@serial (name = c_name)*/
+    private String name;
+
+    public Citys() {
+    }
+
+    public Citys(Integer idCity, String name) {
+        this.idCity = idCity;
+        this.name = name;
+    }
+
+    public Integer getIdCity() {
+        return idCity;
+    }
+
+    public void setIdCity(Integer idCity) {
+        this.idCity = idCity;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Citys)) return false;
+
+        Citys citys = (Citys) o;
+
+        if (idCity != null ? !idCity.equals(citys.idCity) : citys.idCity != null) return false;
+        if (name != null ? !name.equals(citys.name) : citys.name != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = idCity != null ? idCity.hashCode() : 0;
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        return result;
+    }
+
+}
---
+++ my-domain/src/main/java/by/romanovich/it/pojos/users/Streets.java
@@ -0,0 +1,65 @@
+package by.romanovich.it.pojos.users;
+
+import java.io.Serializable;
+
+/**
+ * This is Streets pojo
+ * @author Romanovich Andrei
+ * @version 1.0
+ */
+public class Streets implements Serializable {
+
+    private static final Long serialVersionUID = 25467879401L;
+
+    /**@serial (name = id_street)*/
+    private Integer idStreet;
+
+    /**@serial (name = s_name)*/
+    private String name;
+
+    public Streets() {
+    }
+
+    public Streets(Integer idStreet, String name) {
+        this.idStreet = idStreet;
+        this.name = name;
+    }
+
+    public Integer getIdStreet() {
+        return idStreet;
+    }
+
+    public void setIdStreet(Integer idStreet) {
+        this.idStreet = idStreet;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Streets)) return false;
+
+        Streets streets = (Streets) o;
+
+        if (idStreet != null ? !idStreet.equals(streets.idStreet) : streets.idStreet != null) return false;
+        if (name != null ? !name.equals(streets.name) : streets.name != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = idStreet != null ? idStreet.hashCode() : 0;
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        return result;
+    }
+
+
+}
